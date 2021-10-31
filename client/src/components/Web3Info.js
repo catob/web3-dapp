3233991b82372b48cedab6354cb330e9f7b56f32
cato b <cato.benjaminsen@gmail.com>
2021-10-31 16:12:52 +0100

    Add logic to get account balance

--- client/src/components/Web3Info.js
+++ client/src/components/Web3Info.js
@@ -1,8 +1,21 @@
-import React, { useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 export default function Web3Info(props) {
   const { web3Context } = props;
-  const { networkId, networkName, accounts, providerName } = web3Context;
+  const { networkId, networkName, accounts, providerName, lib } = web3Context;
+
+  const [balance, setBalance] = useState(0);
+
+  const getBalance = useCallback(async () => {
+    let balance = accounts && accounts.length > 0
+      ? lib.utils.fromWei(await lib.eth.getBalance(accounts[0]),  'ether')
+      : 'Unknown';
+    setBalance(balance);
+  }, [accounts, lib.eth, lib.utils]);
+
+  useEffect(() => {
+    getBalance();
+  }, [accounts, getBalance, networkId]);
 
   const requestAuth = async web3Context => {
     try {
@@ -18,6 +31,8 @@ export default function Web3Info(props) {
     <div>
       <h3>{props.title}</h3>
       <div>Network: {networkId ? `${networkId} - ${networkName}` : 'No connection'}</div>
+      <div>Your address: {accounts && accounts.length ? accounts[0] : 'unknown'}</div>
+      <div>Your ETH balance: {balance}</div>
       <div>Provider: {providerName}</div>
       {accounts && accounts.length ? (
         <div>Accounts & Signing Status: Access Grated</div>
