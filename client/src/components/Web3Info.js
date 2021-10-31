fa57ec51aa31ecd04aada960243714890b35f583
cato b <cato.benjaminsen@gmail.com>
2021-10-31 16:01:44 +0100

    Add request to grant access to account

--- client/src/components/Web3Info.js
+++ client/src/components/Web3Info.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 export default function Web3Info(props) {
   const { web3Context } = props;
-  const { networkId, networkName, providerName } = web3Context;
+  const { networkId, networkName, accounts, providerName } = web3Context;
+
+  const requestAuth = async web3Context => {
+    try {
+      await web3Context.requestAuth();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const requestAccess = useCallback(() => requestAuth(web3Context), []);
 
   return (
     <div>
       <h3>{props.title}</h3>
       <div>Network: {networkId ? `${networkId} - ${networkName}` : 'No connection'}</div>
       <div>Provider: {providerName}</div>
+      {accounts && accounts.length ? (
+        <div>Accounts & Signing Status: Access Grated</div>
+      ) : !!networkId && providerName !== 'infura' ? (
+        <>
+          <button onClick={requestAccess}>Request Access</button>
+        </>
+      ) : <></>}
     </div>
   );
 };
